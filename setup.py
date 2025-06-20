d386583f482719c89b96d58b12cb801f6477a373
broadfield-dev <broadfield.apps@gmail.com>
2025-06-19 17:04:54 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
     description='A pure Python library for persistent, semantic chatbot memory.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/broadfield-dev/iLearn-memory',
-    packages=find_packages(include=['imemory', 'imemory.*']),
+    url='https://github.com/broadfield-dev/memai',
+    packages=find_packages(include=['memai', 'memai.*']),
     install_requires=requirements,
     include_package_data=True,
     classifiers=[
