7e658cabb8e5fbf46f95cbc53a7c9942162724fe
broadfield-dev <broadfield.apps@gmail.com>
2025-06-19 17:00:23 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='imemory',
+    version='1.0.0',
+    author='broadfield-dev',
+    description='A pure Python library for persistent, semantic chatbot memory.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/broadfield-dev/iLearn-memory',
+    packages=find_packages(include=['imemory', 'imemory.*']),
+    install_requires=requirements,
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    python_requires='>=3.8',
+)
